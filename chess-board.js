8536f637157c29d9f47d1a86b7440ba94902fc9c
inpechman <inpechman@gmail.com>
2018-06-25 18:04:15 +0300

    chess update

--- chess-board.js
+++ chess-board.js
@@ -1,7 +1,7 @@
-pawn1 = document.createElement("div");
-pawn1.classList.add("pawn");
-pawn1.myPosition = [Math.floor(Math.random()*8), Math.floor(Math.random()*8)];
-document.getElementsByClassName("row")[pawn1.myPosition[0]].getElementsByClassName("cell")[pawn1.myPosition[1]].appendChild(pawn1);
+// pawn1 = document.createElement("div");
+// pawn1.classList.add("white-pawn");
+// pawn1.myPosition = [Math.floor(Math.random() * 8), Math.floor(Math.random() * 8)];
+// document.getElementsByClassName("row")[pawn1.myPosition[0]].getElementsByClassName("cell")[pawn1.myPosition[1]].appendChild(pawn1);
 
 allCells = [];
 for (var i = 0; i < document.getElementsByClassName("row").length; i++) {
@@ -12,35 +12,168 @@ for (var i = 0; i < document.getElementsByClassName("row").length; i++) {
     allCells.push(row);
 }
 console.log(allCells);
+var currentPlayer = 1;
 
-pawn1.onclick = function () {
-    cp = pawn1.myPosition;
-    console.log(this);
-    var optinalPositions = [[cp[0] - 1, cp[1]], [cp[0] + 1, cp[1]], [cp[0], cp[1] - 1], [cp[0], cp[1] + 1]].filter(function (pos) {
-        console.log(Math.max.apply(null, pos));
-        return Math.min.apply(null, pos) >= 0 && Math.max.apply(null, pos) < 8;
-    });
+function changePlayer() {
+    currentPlayer = currentPlayer === 1 ? 2 : 1;
+}
 
-    console.log(optinalPositions);
-    var optinalCells = [];
-    for (var position of optinalPositions) {
-        cell = allCells[position[0]][position[1]];
-        cell.style.background = "rgba(255,99,71,0.5)";
-        cell.onclick = function (position) {
-            return function () {
-                this.appendChild(pawn1);
-                console.log(cp);
+var redPawns = [];
+var whitePawns = [];
 
-                cp[0] = position[0];
-                cp[1] = position[1];
-                for (var cell of optinalCells) {
-                    cell.style["background"] = null;
-                    cell.onclick = null;
-                    console.log(cell)
+for (var i = 0; i < 12; i++) {
+    var redPawn = document.createElement("div");
+    redPawn.classList.add("red-pawn");
+    redPawn.classList.add("pawn");
+    var y = i % 3;
+    var x = i % 4;
+    redPawn.myPosition = [y, x * 2 + (y % 2 ? 0 : 1)];
+    allCells[y][x * 2 + (y % 2 ? 0 : 1)].appendChild(redPawn);
+    redPawns.push(redPawn);
+}
+
+for (var i = 0; i < 12; i++) {
+    var whitePawn = document.createElement("div");
+    whitePawn.classList.add("white-pawn");
+    whitePawn.classList.add("pawn");
+    var y = i % 3;
+    var x = i % 4;
+    whitePawn.myPosition = [y + 5, x * 2 + (y % 2 ? 1 : 0)];
+    allCells[y + 5][x * 2 + (y % 2 ? 1 : 0)].appendChild(whitePawn);
+    whitePawns.push(whitePawn);
+}
+
+var optionalCells = [];
+
+for (var redPawn of redPawns) {
+    redPawn.onclick = pawnClick(1, true);
+}
+
+for (var whitePawn of whitePawns) {
+    whitePawn.onclick = pawnClick(2, false);
+}
+
+
+function resetOptionalCells() {
+    for (var cell of optionalCells) {
+        cell.style.background = null;
+        cell.onclick = null;
+        console.log("reset cells")
+    }
+    optionalCells = []
+}
+
+// pawn1.onclick = function () {
+//     cp = pawn1.myPosition;
+//     console.log(this);
+//     var optionalPositions = [[cp[0] - 1, cp[1]], [cp[0] + 1, cp[1]], [cp[0], cp[1] - 1], [cp[0], cp[1] + 1]].filter(function (pos) {
+//         console.log(Math.max.apply(null, pos));
+//         return Math.min.apply(null, pos) >= 0 && Math.max.apply(null, pos) < 8;
+//     });
+//
+//     console.log(optionalPositions);
+//     resetOptionalCells();
+//     for (var position of optionalPositions) {
+//         var cell = allCells[position[0]][position[1]];
+//         cell.style.background = "rgba(255,99,71,0.5)";
+//         cell.onclick = function (position) {
+//             return function () {
+//                 this.appendChild(pawn1);
+//                 console.log(cp);
+//
+//                 cp[0] = position[0];
+//                 cp[1] = position[1];
+//                 // for (var cell of optionalCells) {
+//                 //     cell.style.background = null;
+//                 //     cell.onclick = null;
+//                 //     console.log(cell)
+//                 // }
+//                 resetOptionalCells();
+//             }
+//         }(position);
+//         optionalCells.push(cell);
+//     }
+//
+// };
+
+function pawnClick(player, backword) {
+    var opponents = ["red", "white"];
+    var playerOpp = opponents[(player === 1) ? 0 : 1];
+    var otherOpp = opponents[(player === 2) ? 0 : 1];
+
+    return function () {
+        if (currentPlayer !== player) {
+            return
+        }
+        var pawn = this;
+        cp = pawn.myPosition;
+        console.log(this);
+        var optionalPositions = [[(backword) ? cp[0] + 1 : cp[0] - 1, cp[1] + 1], [(backword) ? cp[0] + 1 : cp[0] - 1, cp[1] - 1]].filter(function (pos) {
+            console.log(Math.max.apply(null, pos));
+            return Math.min.apply(null, pos) >= 0 && Math.max.apply(null, pos) < 8;
+        });
+
+        console.log(optionalPositions);
+        resetOptionalCells();
+        for (var position of optionalPositions) {
+            var cell = allCells[position[0]][position[1]];
+            var cellChild = cell.getElementsByClassName("pawn")[0];
+            if (cellChild !== undefined) {
+                if (cellChild.classList[0] === otherOpp + "-pawn") {
+                    console.log(cellChild);
+                    var direction = cellChild.myPosition[1] > cp[1];
+                    var nextPos = [backword ? cp[0] + 2 : cp[0] - 2, direction ? cp[1] + 2 : cp[1] - 2];
+                    var nextCell = allCells[nextPos[0]] [nextPos[1]];
+                    var nextCellChild = nextCell.getElementsByClassName("pawn")[0];
+                    if (nextCellChild !== undefined) {
+                        continue
+                    }
+                    nextCell.style.background = "rgba(255,99,71,0.5)";
+                    nextCell.onclick = function (position) {
+                        return function () {
+                            this.appendChild(pawn);
+                            document.getElementById(otherOpp + "-dead").appendChild(cellChild);
+                            console.log(cp);
+
+                            cp[0] = position[0];
+                            cp[1] = position[1];
+                            // for (var cell of optionalCells) {
+                            //     cell.style.background = null;
+                            //     cell.onclick = null;
+                            //     console.log(cell)
+                            // }
+                            resetOptionalCells();
+                            changePlayer();
+                        }
+                    }(nextPos);
+                    optionalCells.push(nextCell);
+                    continue;
+
+                } else {
+                    continue;
                 }
+
             }
-        }(position);
-        optinalCells.push(cell);
+            cell.style.background = "rgba(255,99,71,0.5)";
+            cell.onclick = function (position) {
+                return function () {
+                    this.appendChild(pawn);
+                    console.log(cp);
+
+                    cp[0] = position[0];
+                    cp[1] = position[1];
+                    // for (var cell of optionalCells) {
+                    //     cell.style.background = null;
+                    //     cell.onclick = null;
+                    //     console.log(cell)
+                    // }
+                    resetOptionalCells();
+                    changePlayer();
+                }
+            }(position);
+            optionalCells.push(cell);
+        }
+
     }
 
-};
+}
